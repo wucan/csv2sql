24ec3186867097ab028699b3e8de02d9e33cefce
Can Wu <wu.canus@gmail.com>
2012-03-28 11:32:41 +0800

    preferences: manage settings with QSettings
    I use ini file, the path in linux is ~/.config/adidas/csv2sql.ini

--- preferences.cpp
+++ preferences.cpp
@@ -1,4 +1,5 @@
 #include <QPushButton>
+#include <QSettings>
 
 #include "preferences.h"
 
@@ -14,6 +15,22 @@ PreferencesDialog::PreferencesDialog()
     connect(ui.buttonBox->buttons().at(0), SIGNAL(clicked()), this, SLOT(ok()));
     connect(ui.buttonBox->buttons().at(1), SIGNAL(clicked()), this, SLOT(close()));
     ui.buttonBox->buttons().at(1)->setFocus();
+
+    /*
+     * restore settings
+     */
+    QSettings settings(QSettings::IniFormat, QSettings::UserScope,
+                       "adidas", "csv2sql");
+    settings.beginGroup("csv");
+    ui.lineEdit_csv_path->setText(settings.value("path", QCoreApplication::applicationDirPath()).toString());
+    ui.lineEdit_csv_interval->setText(settings.value("interval", 10).toString());
+    settings.endGroup();
+    settings.beginGroup("db");
+    ui.lineEdit_db_host->setText(settings.value("host", "127.0.0.1").toString());
+    ui.lineEdit_db_name->setText(settings.value("name", "adidas").toString());
+    ui.lineEdit_db_user->setText(settings.value("user", "adidas").toString());
+    ui.lineEdit_db_password->setText(settings.value("password", "adidas").toString());
+    settings.endGroup();
 }
 
 void PreferencesDialog::close()
@@ -23,5 +40,22 @@ void PreferencesDialog::close()
 
 void PreferencesDialog::ok()
 {
+    /*
+     * store settings
+     */
+    QSettings settings(QSettings::IniFormat, QSettings::UserScope,
+                       "adidas", "csv2sql");
+    settings.beginGroup("csv");
+    settings.setValue("path", ui.lineEdit_csv_path->text());
+    settings.setValue("interval", ui.lineEdit_csv_interval->text());
+    settings.endGroup();
+
+    settings.beginGroup("db");
+    settings.setValue("host", ui.lineEdit_db_host->text());
+    settings.setValue("name", ui.lineEdit_db_name->text());
+    settings.setValue("user", ui.lineEdit_db_user->text());
+    settings.setValue("password", ui.lineEdit_db_password->text());
+    settings.endGroup();
+
     done(0);
 }
