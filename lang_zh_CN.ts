19d31995e9c354dff80b92b34daf33dee7cb53f8
Can Wu <wu.canus@gmail.com>
2012-03-30 10:30:31 +0800

    add the translator file

---
+++ lang_zh_CN.ts
@@ -0,0 +1,226 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="33"/>
+        <source>google:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="63"/>
+        <source>&amp;File</source>
+        <translation type="unfinished">文件</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="70"/>
+        <source>&amp;Edit</source>
+        <translation type="unfinished">编辑</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="77"/>
+        <source>&amp;Help</source>
+        <translation type="unfinished">帮助</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>E&amp;xit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="114"/>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished">设置</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>&amp;Start</source>
+        <translation type="unfinished">启动</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="144"/>
+        <location filename="mainwindow.ui" line="147"/>
+        <source>Iconize</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <source>&amp;Register</source>
+        <translation type="unfinished">注册</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="36"/>
+        <location filename="mainwindow.cpp" line="181"/>
+        <source>CSV2SQL</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="130"/>
+        <source>Mi&amp;nimize</source>
+        <translation type="unfinished">最小化窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="133"/>
+        <source>Ma&amp;ximize</source>
+        <translation type="unfinished">最大化窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="136"/>
+        <source>&amp;Restore</source>
+        <translation type="unfinished">恢复窗口</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="139"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">退出</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="181"/>
+        <source>Systray</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PreferencesDialog</name>
+    <message>
+        <location filename="preferences.cpp" line="103"/>
+        <source>Choose CSV Directory</source>
+        <translation type="unfinished">请选择存放CSV的文件夹</translation>
+    </message>
+</context>
+<context>
+    <name>RegisterDialog</name>
+    <message>
+        <location filename="registerdialog.cpp" line="10"/>
+        <source>Register the Software</source>
+        <translation type="unfinished">软件注册</translation>
+    </message>
+    <message>
+        <location filename="registerdialog.cpp" line="18"/>
+        <source>Serial Number</source>
+        <translation type="unfinished">序列号</translation>
+    </message>
+    <message>
+        <location filename="registerdialog.cpp" line="23"/>
+        <source>Register Number</source>
+        <translation type="unfinished">注册号</translation>
+    </message>
+    <message>
+        <location filename="registerdialog.cpp" line="32"/>
+        <source>Register</source>
+        <translation type="unfinished">注册</translation>
+    </message>
+    <message>
+        <location filename="registerdialog.cpp" line="33"/>
+        <source>Close</source>
+        <translation type="unfinished">关闭</translation>
+    </message>
+</context>
+<context>
+    <name>aboutWidget</name>
+    <message>
+        <location filename="about.ui" line="14"/>
+        <source>About</source>
+        <translation type="unfinished">关于</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="24"/>
+        <source>Version</source>
+        <translation type="unfinished">版本</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="91"/>
+        <source>Authors</source>
+        <translation type="unfinished">作者</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="105"/>
+        <source>Thanks</source>
+        <translation type="unfinished">鸣谢</translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="111"/>
+        <source>Thank to all the helpers.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="about.ui" line="129"/>
+        <source>Licence</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>preferencesDialog</name>
+    <message>
+        <location filename="preferences.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="46"/>
+        <source>General</source>
+        <translation type="unfinished">一般选项</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="61"/>
+        <source>City</source>
+        <translation type="unfinished">城市</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="86"/>
+        <source>CSV</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="101"/>
+        <source>Path</source>
+        <translation type="unfinished">路径</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="111"/>
+        <source>Interval</source>
+        <translation type="unfinished">间隔</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="134"/>
+        <source>Browse...</source>
+        <translation type="unfinished">流览...</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="143"/>
+        <source>Database</source>
+        <translation type="unfinished">数据库</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="158"/>
+        <source>Host</source>
+        <translation type="unfinished">主机</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="168"/>
+        <source>User</source>
+        <translation type="unfinished">用户名</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="175"/>
+        <source>Password</source>
+        <translation type="unfinished">密码</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="182"/>
+        <source>Name</source>
+        <translation type="unfinished">名字</translation>
+    </message>
+</context>
+</TS>
